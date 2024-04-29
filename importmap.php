561b8be3d6a1bed2d8e82c5008f76821b9d8e2dd
Michael Lachand <lachand_michael@hotmail.com>
2024-04-29 15:26:13 +0200

    Ajout Bootstrap

--- importmap.php
+++ importmap.php
@@ -25,4 +25,8 @@ return [
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.3',
+        'type' => 'css',
+    ],
 ];
